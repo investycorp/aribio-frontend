0e9be15c905b38783d21964d78cd1cbdf704f163
Lim Chang-Hyun <stablestaple92@gmail.com>
2024-01-08 16:53:19 +0900

    Fix : Fix Notice list, Press List

--- src/hooks/irpr/useNoticeList.js
+++ src/hooks/irpr/useNoticeList.js
@@ -3,8 +3,14 @@ import useLanguageQuery from '../useLanguageQuery';
 
 const fetchNoticeList = (keyWord, language, pageNumber) => {
   const langParam = language !== 'KOR' ? 'ENGLISH' : 'KOREAN';
+  const params = {
+    ...(keyWord && {keyword: keyWord}), // keyword가 있을 때만 포함시킵니다.
+    language: langParam,
+    pageNo: pageNumber,
+  };
+
   return axiosInstance.get(`/user/notice`, {
-    params: {keyword: keyWord, language: langParam, pageNo: pageNumber},
+    params,
   });
 };
 
